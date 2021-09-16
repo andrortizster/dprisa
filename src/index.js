49a820b5a0d932312bc46c5bbb49aadae1282ead
andrortizster <andrortizster@gmail.com>
2021-09-15 22:18:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reportWebVitals from './reportWebVitals';
+import reducer from './store/reducer';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider >
+    <Provider store={store}>
       <App />
     </Provider>, 
 document.getElementById('root'));
@@ -14,4 +20,5 @@ document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+reportWebVitals();
 serviceWorker.unregister();
