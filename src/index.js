9ab412eb8d4709f7635879f1081a9c42ebeb2782
androtizster <androtizster@gmail.com>
2021-09-17 21:36:30 -0400

    Cambios en la visualidad de la página

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reducer from './store/reducer';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>, 
-document.getElementById('root'));
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.unregister();
